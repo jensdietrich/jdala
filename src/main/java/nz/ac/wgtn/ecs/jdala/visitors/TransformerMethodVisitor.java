9318c667d0f3365a46bf8ef3ecd3bcb874ddd727
Quinten Smit <50760069+qwsmit@users.noreply.github.com>
2025-02-04 16:43:35 +1300

    Cache variables so that validate write can be done after super constructor call

--- src/main/java/nz/ac/wgtn/ecs/jdala/visitors/TransformerMethodVisitor.java
+++ src/main/java/nz/ac/wgtn/ecs/jdala/visitors/TransformerMethodVisitor.java
@@ -16,11 +16,14 @@ public class TransformerMethodVisitor extends MethodVisitor {
     private final String superClassName;
     private final String methodDescriptor;
 
+    private int varCounter = 0;
+
     public TransformerMethodVisitor(MethodVisitor methodVisitor, String superClassName, String descriptor, Set<AnnotationPair> annotations, String classPath) {
         super(Opcodes.ASM9, methodVisitor);
         this.superClassName = superClassName;
         this.classPath = classPath;
         this.annotations = annotations;
+
         this.methodDescriptor = descriptor;
         int count = methodDescriptor.length() - methodDescriptor.replace(";", "").length();
 
@@ -28,7 +31,6 @@ public class TransformerMethodVisitor extends MethodVisitor {
             this.superConstructorCalled = true;
         }
 
-//        System.out.println("New TransformerMethodVisitor: " + classPath + " annotation size: " + annotations.size());
     }
 
     @Override
@@ -59,39 +61,56 @@ public class TransformerMethodVisitor extends MethodVisitor {
 
     @Override
     public void visitMethodInsn(int opcode, String owner, String name, String descriptor, boolean isInterface) {
+        super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
         if (opcode == Opcodes.INVOKESPECIAL && name.equals("<init>") && owner.equals(superClassName)) {
             superConstructorCalled = true;
+            if (varCounter > 0){
+                for (int var = 0; var < varCounter; var += 2) {
+
+                    mv.visitVarInsn(Opcodes.ALOAD, 11 + var); // Load object
+                    mv.visitVarInsn(Opcodes.ALOAD, 10 + var);  // Load value
+
+                    super.visitMethodInsn(
+                            Opcodes.INVOKESTATIC,
+                            "nz/ac/wgtn/ecs/jdala/JDala",
+                            "validateWrite",
+                            "(Ljava/lang/Object;Ljava/lang/Object;)V",
+                            false
+                    );
+                }
+            }
         }
-        super.visitMethodInsn(opcode, owner, name, descriptor, isInterface);
     }
 
     @Override
     public void visitFieldInsn(int opcode, String owner, String name, String descriptor) {
-//        System.out.println(descriptor);
-//        if ((opcode == Opcodes.PUTFIELD || opcode == Opcodes.GETFIELD) && (descriptor.startsWith("L") || descriptor.startsWith("["))) {
-//            hasFieldCall = true;
-//        }
-
-        if ((descriptor.startsWith("L") || descriptor.startsWith("[")) && superConstructorCalled) {
+        if ((descriptor.startsWith("L") || descriptor.startsWith("["))) {
             if (opcode == Opcodes.PUTFIELD) {
-                injectWriteValidator();
+                if (superConstructorCalled) {
+                    injectWriteValidator();
+                } else {
+                    System.out.println(classPath + " " + name + " " + descriptor + " " + varCounter);
+
+                    // Store the value (assuming it's an Object)
+                    mv.visitVarInsn(Opcodes.ASTORE, 10 + varCounter);
+                    // Store the object reference
+                    mv.visitVarInsn(Opcodes.ASTORE, 11 + varCounter);
+
+
+                    mv.visitVarInsn(Opcodes.ALOAD, 11 + varCounter);
+                    mv.visitVarInsn(Opcodes.ALOAD, 10 + varCounter);
+                    varCounter += 2;
+                }
             } else if (opcode == Opcodes.GETFIELD) { // Needs to be added after field has been retrieved
-                super.visitFieldInsn(opcode, owner, name, descriptor);
-                injectReadValidator();
-                return;
+                if (superConstructorCalled) {
+                    super.visitFieldInsn(opcode, owner, name, descriptor);
+                    injectReadValidator();
+                    return;
+                }
             }
         }
         super.visitFieldInsn(opcode, owner, name, descriptor);
     }
-
-//    @Override
-//    public void visitInsn(int opcode) {
-//        if (opcode == Opcodes.RETURN && superConstructorCalled) {
-//            injectConstructorValidator();
-//        }
-//        super.visitInsn(opcode);
-//    }
-
 
     private void injectRegister(String methodName, int varIndex) {
         // Load the variable onto the stack
